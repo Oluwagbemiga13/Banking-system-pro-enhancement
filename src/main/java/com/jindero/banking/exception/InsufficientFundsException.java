705ae52756dcc4d57be5ad16081396c82f93a4e8
Jindrich Ovadek <jinova95@seznam.cz>
2025-06-18 14:34:53 +0200

    add custom exceptions

---
+++ src/main/java/com/jindero/banking/exception/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.jindero.banking.exception;
+
+public class InsufficientFundsException extends RuntimeException {
+
+  public InsufficientFundsException(String message) {
+    super(message);
+  }
+}
+
