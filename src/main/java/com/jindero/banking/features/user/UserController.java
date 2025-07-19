10e957c67057f5ed054b68877d60947a824f70a2
Jindrich Ovadek <jinova95@seznam.cz>
2025-07-19 18:38:29 +0200

    "feat: complete UserController with full CRUD REST API"

--- src/main/java/com/jindero/banking/features/user/UserController.java
+++ src/main/java/com/jindero/banking/features/user/UserController.java
@@ -1,6 +1,7 @@
 package com.jindero.banking.features.user;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +13,49 @@ import java.util.Optional;
 public class UserController {
 
   private final UserService userService;
-  private final UserRepository userRepository;
 
   //Konstruktor
-  public UserController(UserService userService, UserRepository userRepository){
+  public UserController(UserService userService){
     this.userService = userService;
-    this.userRepository = userRepository;
   }
 
+  //Get all
   @GetMapping("")
   public List<User> getAllUsers(){
     return userService.findAll();
   }
 
+  // Get podle id
   @GetMapping("/{id}")
-  public User getUserById(@PathVariable Long id){
+  public ResponseEntity<User> getUserById(@PathVariable Long id){
     Optional<User> user = userService.findById(id);
 
     if (user.isPresent()){
-      return user.get();
+      return ResponseEntity.ok(user.get());
     } else {
-      return null;
+      return ResponseEntity.notFound().build();
     }
   }
 
+  // Post create
   @PostMapping("")
   public ResponseEntity<User> createUser(@RequestBody User user){
     User creatUser = userService.createUser(user);
-    return ResponseEntity.ok(creatUser);
+    return ResponseEntity.status(HttpStatus.CREATED).body(creatUser);
+  }
+
+  //Put update
+  @PutMapping("/{id}")
+  public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
+    User updateUser = userService.updateUser(id,user);
+    return ResponseEntity.ok(updateUser);
+  }
+
+  //Delete user
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteUser(@PathVariable Long id){
+    userService.deleteUser(id);
+    return ResponseEntity.ok("User deleted successfully");
   }
 
 }
